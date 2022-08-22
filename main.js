4d42aa3cc9f73daff217407b9a4802f29b70e862
Adigun Olawale <101121123+Gaius388@users.noreply.github.com>
2022-08-22 20:51:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// const tabContent = document.getElementsByClassName("tabcontent");
+// const tabLinks = document.getElementsByClassName("tabLink");
+// const btn1 = document.getElementById('btn1')
+// const btn2 = document.getElementById('btn2')
+// const btn3 = document.getElementById('btn3')
+
+// function tabOpen(one, two, three) {
+//     for(let i = 0; i < tabContent.length; i++) {
+//         tabContent[i].style.display = "none";
+//     };
+//     for(let i = 0; i < tabLinks.length; i++) {
+//         tabLinks[i].style.backgroundColor = "";
+//     };
+//     document.getElementById(one).style.display = "block";
+//     two.style.backgroundColor = three;
+// };
+// function tabOpen (one) {
+//     document.getElementById(one).style.display = 'block'
+// }
+// btn1.addEventListener('click', () => {
+//     tabOpen('first')
+// })
+// btn2.addEventListener('click', () => {
+//     tabOpen('second')
+// })
+// btn3.addEventListener('click', () => {
+//     tabOpen('third')
+//})
+// tabOpen('second', tabLinks[1], 'orange')
+// tabOpen('third', tabLinks[2], 'orange')
